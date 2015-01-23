1a448cf8dc472fdd0672160f5e253413586b9d82
ionization <ionization@localhost.localdomain>
2015-01-24 03:11:06 +0700

    Add glib

--- index.c
+++ index.c
@@ -1,18 +1,19 @@
 #include "stdio.h"
 #include "string.h"
 #include <glib.h>
-#include "hash.c"
-
-
-int fastHash(const char * data,int max){
-	int hash=SuperFastHash(data,strlen(data));
-	return hash%max;
-}
 
 int main(int argc, char const *argv[])
 {
 	printf("Hello param: %s\n",argv[1]);
-	int newK=fastHash(argv[1],20);//SuperFastHash(argv[1],strlen(argv[1]));
-	printf("newK %d\n",newK);
+
+	GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
+	g_hash_table_insert(hash, "Virginia", "Richmond");
+	g_hash_table_insert(hash, "Texas", "Austin");
+	g_hash_table_insert(hash, "Ohio", "Columbus");
+	printf("There are %d keys in the hash\n", g_hash_table_size(hash));
+	printf("The capital of Texas is %s\n", g_hash_table_lookup(hash, "Texas"));
+	gboolean found = g_hash_table_remove(hash, "Virginia");
+	printf("The value 'Virginia' was %sfound and removed\n", found ? "" : "not ");
+	g_hash_table_destroy(hash);
 	return 0;
 }

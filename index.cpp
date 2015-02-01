7e60a92f06995095e58894027ab9764174f16eef
jigkoxsee <ked.naja@gmail.com>
2015-02-01 23:15:33 +0700

    v.1 Map

--- index.cpp
+++ index.cpp
@@ -3,6 +3,7 @@
 #include <dirent.h>
 //#include <errno.h>
 #include <list>
+#include <map>
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -10,36 +11,60 @@
 
 using namespace std;
 
+
+
+/*
+Node i1= Node("hello");
+i1.add(1);
+i1.add(2);
+i1.add(3);
+cout<<i1.getAll()<<endl;
+*/
 class Node
 {
 public:
     string word;
-    list<int> file;
+    list<string> filename;
     Node(string w){
         word=w;
     }
 
-    bool add(int fn){
-        //list<int>::iterator it=mylist.end()
-        //.empty()
-        if(file.empty()||file.back()!=fn){
-            file.push_back(fn);
+    bool add(string fn){
+        if(filename.empty()||filename.back()!=fn){
+            filename.push_back(fn);
             return true;
         }
         return false;
     }
     string getAll(){
-        string all=word+":"+to_string(file.size())+":";
-        for (list<int>::iterator it = file.begin(); it != file.end(); it++){
-            all+=to_string(*it);
-            all+=",";
-        }
-
+        string all=word+":"+to_string(filename.size())+":";
+        for (list<string>::iterator it = filename.begin(); it != filename.end(); it++)
+            all+=(*it)+",";
         return all.substr(0, all.size()-1);
     }
 
     //~Node();
 };
+
+map <string, Node *> allmap;
+void indexing(string word,string filename){
+    map<string, Node *>::iterator it;
+    it=allmap.find(word);
+    Node *n;
+    if(it!=allmap.end()){
+        //cout<<"In:"<<(it->second)->getAll()<<endl;
+        n = it->second;
+        //n->add(filename);
+
+     //   cout<<"2:"<<(it->second)->getAll()<<endl;
+    }else{
+        n = new Node(word);
+    }
+        n->add(filename);
+        allmap[word]=n;
+        //cout<<"1:"<<n->getAll()<<endl;
+
+}
 
 /*function... might want it in some class?*/
 /*
@@ -58,20 +83,24 @@ int fileCount (string dir)
         cout<<"Error : Can't open directory"<<endl;
         exit(1);
     }
+
+
+
     while ((dirp = readdir(dp)) != NULL) {
         //count++;
         //-----------------------------
         filename=dir+"/file"+to_string(++count)+".txt";
-        cout<<filename<<endl;
+        //cout<<filename<<endl;
         infile.open(filename);
         while(1)
         {
-            //getline(infile, word);
             infile>>word;
             if (infile.fail()||infile.eof())
+
                 break;
-            cout << word << endl;
+            //cout << word << endl;
             // Indexing
+            indexing(word,to_string(count));
         }
         infile.close();
         //-----------------------------
@@ -84,17 +113,17 @@ int main(int argc, char* const argv[])
 {
     string dir = string(argv[1]);
     
-    //cout<<dir<<endl;
+    cout<<dir<<endl;
+
     int count=fileCount(dir);
     cout<<"Count"<<count<<endl;
-    Node i1= Node("hello");
-    i1.add(1);
-    i1.add(2);
-    i1.add(3);
-    cout<<i1.getAll()<<endl;
 
 
 //-----------------------------------
-    
+
+    cout<<"map"<<allmap.size()<<endl;
+    /*for (auto& x: allmap) {
+        std::cout <<(x.second)->getAll() << endl;
+    }*/
     return 0;
 }

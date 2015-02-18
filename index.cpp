8c339d2ce79dd70d0e71b8ffd8e9b2873eee8923
root <root@localhost.0x5A696B6F>
2015-02-18 16:08:24 +0700

    thread read file

--- index.cpp
+++ index.cpp
@@ -1,8 +1,6 @@
 #include <cstdlib>
 #include <dirent.h>
-#include <forward_list>
-#include <list>
-#include <queue>
+#include <vector>
 #include <map>
 #include <unordered_map>
 #include <string>
@@ -11,19 +9,20 @@
 #include <sstream>
 #include <thread>
 #include <algorithm>
+#include <mutex>
 
 #include <ctime>
 #include <ratio>
 #include <chrono>
 
 using namespace std;
-
+mutex mtex;
 bool haveNext=true;
-
+string dir;
 class Node
 {
 public:
-  list<int> filename;
+  vector<int> filename;
   Node(){
   }
   bool add(int fn){
@@ -36,7 +35,7 @@ public:
 
   string getAll(){
     string all=":"+to_string(filename.size())+":";
-    for (list<int>::iterator it = filename.begin(); it != filename.end(); it++)
+    for (vector<int>::iterator it = filename.begin(); it != filename.end(); it++)
       all+=to_string(*it)+",";
     return all.substr(0, all.size()-1);
   }
@@ -50,21 +49,24 @@ char easytolower(char in){
 }
 
 
-queue<pair<string,int>> word_queue;
+vector<pair<string,int>> word_queue[4];
 
 static unordered_map<string, Node*> allmap;
 static chrono::duration<double> time_span;
 
 void indexing(string word,int filename){
   transform(word.begin(), word.end(), word.begin(), easytolower);
+  /*
   Node *n = new Node();
   n->add(filename);
+  
   pair<unordered_map<string, Node*>::iterator, bool>  status=allmap.emplace(word,n);
 
   if(!status.second){
     allmap[word]->add(filename);
     delete n;
-  }
+  }*/
+
 }
 
 bool isAlphabet(char ch){
@@ -77,12 +79,12 @@ bool isAlphabet(char ch){
 /*
 List file and read it to 
 */
-void fileRead(string dir,int count)
+void fileRead(int start,int end,int thread)
 {
   string filename;
   string word="";
   ifstream infile;
-  for (int i = 1; i <= count; i++)
+  for (int i = start; i <= end; i++)
   {
     filename=dir+"/file"+to_string(i)+".txt";
     infile.open(filename.c_str());
@@ -93,8 +95,9 @@ void fileRead(string dir,int count)
         word +=ch;
       }else{
         if(word.size()>0){
-//          indexing(word,i);
-          word_queue.push(make_pair(word,i));
+//          mtex.lock();
+          word_queue[thread].push_back(make_pair(word,i));
+//          mtex.unlock();
         }
         word="";
       }
@@ -119,20 +122,24 @@ int fileCount (string dir)
   return count-2;
 }
 
+/*
 void job2(){
   while(haveNext||!word_queue.empty()){
     if(!word_queue.empty()){
       pair<string,int> w=word_queue.front();
-//      indexing(w.first,w.second);
+//      mtex.lock();
+      indexing(w.first,w.second);
+//      mtex.unlock();
      word_queue.pop();
     }
   }
 }
+*/
 
 int main(int argc, char* const argv[])
 {
-  thread job(job2);
-  string dir = string(argv[1]);
+ // thread job(job2);
+  dir = string(argv[1]);
   cout<<dir<<endl;
   int count=fileCount(dir);
   if(count>3000)
@@ -143,9 +150,23 @@ int main(int argc, char* const argv[])
       allmap.reserve(400000);
   }
 
-  fileRead(dir,count);
-  job.join();
+//  fileRead(dir,count);
+  int part=count/4;
+  thread fr1(fileRead,1,part,1);
+  thread fr2(fileRead,1+part,part*2,2);
+  thread fr3(fileRead,1+part*2,part*3,3);
+  thread fr4(fileRead,1+part*3,count,4);
+  fr1.join();
+  fr2.join();
+  fr3.join();
+  fr4.join();
 
+  cout<<"1 "<<word_queue[1].size()<<endl;
+  cout<<"2 "<<word_queue[2].size()<<endl;
+  cout<<"3 "<<word_queue[3].size()<<endl;
+  cout<<"4 "<<word_queue[4].size()<<endl;
+
+/*
   ofstream of;
   of.open("output");
   of<<allmap.size()<<endl;
@@ -156,12 +177,6 @@ int main(int argc, char* const argv[])
     of<<(it->second)->getAll()<<endl;
   }
   of.close();
-
-  chrono::high_resolution_clock::time_point t1=chrono::high_resolution_clock::now();
-  chrono::high_resolution_clock::time_point t2=chrono::high_resolution_clock::now();
-
-  time_span=chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-  cout<<time_span.count()<<endl;
-
+*/
   return 0;
 }
